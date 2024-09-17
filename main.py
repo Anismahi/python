b6e17a4d73ec2aab9b59c293d9b08c12b97e92ad
Anismahi <mahianis58@gmail.com>
2024-09-17 13:30:14 +0200

    Update main.py

--- main.py
+++ main.py
@@ -18,4 +18,45 @@ print (lpp(2,8))
 def valeur_absolue(x):
     if x<0 :
       return x*(-1)
-print (valeur_absolue(-5))          
+print (valeur_absolue(-5))  
+
+#EXERCICE 10
+def est_pair (n):
+    if n%2:
+        return False
+    else:
+        return True
+print (est_pair(0))
+
+#EXERCICE 12
+def signe (x):
+    if x>0:
+        return "Positif"
+    elif x==0:
+        return "Nulle"
+    elif x<0:
+        return "Negative"
+print (signe(5))
+
+#EXERCICE 
+def suite(n):
+    u = 1
+    for k in range(n):
+        u = 2*u+k
+    return u
+print (suite(2))
+
+#lundi 16 septembre
+def find_roots(a,b,c):
+    D=b**2 - 4*a*c
+    if D>0:
+        root1=(-b+D**0.5)/2*a 
+        root2=(-b-D**0.5)/2*a
+        return [root1,root2]
+    elif D==0:
+        root3=-b/2*a 
+        return root3
+    else:
+        return None
+        
+print(find_roots(6,6,1))
